ad4053b0afb696e57e0ab232ed30c98d30b0e485
MarieDeSmedt <desmedtmarie@outlook.com>
2021-05-07 12:14:34 +0200

    commit recommandation

--- app/myapp.py
+++ app/myapp.py
@@ -1,20 +1,24 @@
 import sys
-sys.path.insert(0,"/home/apprenant/PycharmProjects/NLP_foodflix")
+sys.path.insert(0, "/home/apprenant/PycharmProjects/NLP_foodflix")
 
 import streamlit as st
 import pandas as pd
 
-from src.recommandation  import reco
+from src.recommandation import get_recommandation
 
 st.title("Moteur de recommandation:")
 
 # Importing the dataset
 df = pd.read_csv('/home/apprenant/PycharmProjects/NLP_foodflix/data/02_intermediate/only_string.csv')
-df = df.rename(columns={ df.columns[0]: "index" })
-df['content'] = df[['product_name','brands','generic_name','categories']].astype(str).apply(lambda x: ' // '.join(x), axis = 1)
+df = df.rename(columns={df.columns[0]: "index"})
+df['content'] = df[['product_name', 'brands', 'generic_name', 'categories']].astype(str).apply(lambda x: ' // '.join(x),
+                                                                                               axis=1)
 # Fillna
-df['content'].fillna('Null', inplace = True)
+df['content'].fillna('Null', inplace=True)
 
-user_input = st.text_input('recherche')
+new_input = st.text_input('recherche')
 
-reco(user_input,df)
+results = get_recommandation(new_input,df)
+if new_input != "":
+    for i in range(4):
+        st.write(df['product_name'].iloc[results[i][1]])
--- src/recommandation.py
+++ src/recommandation.py
@@ -1,20 +1,13 @@
-# Importing the libraries
-import pandas as pd
-
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel,cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
 
 
-def reco(user_input,df):
-    vectorizer = TfidfVectorizer(analyzer = 'word', ngram_range = (1, 2), min_df = 0, stop_words = 'english')
-    vectors_content = vectorizer.fit_transform(df['content'])
-    vectors_product = vectorizer.transform([user_input])
-    cosine_similarities = linear_kernel(vectors_product,vectors_content)
-    results = {}
+def get_recommandation(user_input, df):
+    vec = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
+    vectors_content = vec.fit_transform(df['content'])
+    vectors_product = vec.transform([user_input])
+    cosine_similarities = linear_kernel(vectors_product, vectors_content)
+    results={}
     similar_indices = cosine_similarities[0].argsort()[:-10:-1]
-    similar_items = [(cosine_similarities[0][i], df['index'][i]) for i in similar_indices]
-    results = reco(user_input)
-    # on affiche les produits similaires
-    for i in range(4):
-        print(df.iloc[results[i][1]])
-        print("----------------------")
+    results = [(cosine_similarities[0][i], df['index'][i]) for i in similar_indices]
+    return results
