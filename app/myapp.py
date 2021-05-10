63dbfe4473eb77500f016719671a2f64e4a765d6
MarieDeSmedt <desmedtmarie@outlook.com>
2021-05-10 13:33:52 +0200

    upload myapp

--- app/myapp.py
+++ app/myapp.py
@@ -3,8 +3,27 @@ sys.path.insert(0, "/home/apprenant/PycharmProjects/NLP_foodflix")
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-from src.recommandation import get_recommandation, get_idea
+from src.recommandation import get_recommandation, get_idea, get_bert
+
+def display_result(title,col_list,results):
+    st.title(title)
+    for i in range(4):
+        nb = str(i + 1)
+        my_expander = st.beta_expander("proposition n°" + nb, expanded=False)
+        with my_expander:
+            for col in col_list:
+                if title == "Et avec bert?":
+                    dis = results[i][1]
+                    st.write(dis)
+                else:
+                    dis = df[col].iloc[results[i][1]]
+                    if dis:
+                        st.subheader(col, " : ")
+                        st.write(dis)
+
+# #####################################################################""
 
 # Importing the dataset
 df = pd.read_csv('/home/apprenant/PycharmProjects/NLP_foodflix/data/02_intermediate/only_string.csv')
@@ -12,7 +31,7 @@ df = df.rename(columns={df.columns[0]: "index"})
 df['content'] = df[['product_name', 'brands', 'generic_name', 'categories']].astype(str).apply(lambda x: ' // '.join(x),
                                                                                                axis=1)
 df['content'].fillna('Null', inplace=True)
-
+col_list = ['product_name', 'brands', 'generic_name', 'categories']
 # ##########################################################################################"
 #header
 st.header("Démonstration NLP:")
@@ -29,17 +48,31 @@ if new_input :
     if tool == "TfidfVectorizer":
         results = get_recommandation(new_input, df)
         title = "Vous aimez le " + new_input + "?"
+        display_result(title, col_list, results)
+
     elif tool == "CountVectorizer":
         results = get_idea(new_input, df)
         title = "Mais vous avez envie de changement?"
+        display_result(title, col_list, results)
+
     else:
-        title = ("et Bert?")
+        results = get_bert(new_input, df)
+        title = "Et avec bert?"
+        for i in range(4):
+            nb = str(i + 1)
+            my_expander = st.beta_expander("proposition n°" + nb, expanded=False)
+            with my_expander:
+                dis = results[i][1]
+                st.write(dis)
 
 
-    st.title(title)
-    for i in range(4):
-        st.write(df['product_name'].iloc[results[i][1]])
-        st.write(df['brands'].iloc[results[i][1]])
-        st.write(df['generic_name'].iloc[results[i][1]])
-        st.write(df['categories'].iloc[results[i][1]])
-        st.balloons()
+
+
+
+
+
+
+
+
+
+
--- src/recommandation.py
+++ src/recommandation.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
+from sentence_transformers import SentenceTransformer, models
+
 
 def get_recommandation(user_input, df):
     vec = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
@@ -21,3 +23,22 @@ def get_idea(user_input, df):
     similar_indices = cosine_similarities[0].argsort()[:-10:-1]
     results_count = [(cosine_similarities[0][i], df['index'][i]) for i in similar_indices]
     return results_count
+
+def get_bert(user_input, df):
+    mymodel = models.Transformer('camembert-base')
+    pooling_models = models.Pooling(mymodel.get_word_embedding_dimension(),
+                                    pooling_mode_mean_tokens=True,
+                                    pooling_mode_max_tokens=False)
+    model = SentenceTransformer(modules=[mymodel, pooling_models])
+
+    sentence = [user_input]
+    sentences = df['content'].head(200).tolist()
+
+    sentence_embeddings = model.encode(sentences)
+    sentence_emb = model.encode(sentence)
+
+    cosine_similarities = linear_kernel(sentence_emb, sentence_embeddings)
+    results = {}
+    similar_indices = cosine_similarities[0].argsort()[:-5:-1]
+    results = [(cosine_similarities[0][i], sentences[i]) for i in similar_indices]
+    return results
